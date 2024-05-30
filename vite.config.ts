6de6c2ab70809a6f9d6376d9a2070b899276f75a
wbj <709965309@qq.com>
2024-05-30 15:14:32 +0800

    第一次初始化代码

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+
+/**
+ * 一般用来配置我们服务的一些配置东西
+ * 比如代理,启动项目的路径名称,启动项目的页面,端口号
+ */
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueJsx(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
